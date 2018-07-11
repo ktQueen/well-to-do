9a193ce7feb8e453c21a93ccccc0e36c46be42c8
kangting <kangting@daojia.com>
2018-07-11 15:51:06 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Hello from './views/Hello';
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: \.PingFangSC-Light,Helvetica Neue,Helvetica,Arial,PingFang SC,Heiti SC,Hiragino Sans GB,Microsoft YaHei,sans-serif;
+}
+</style>
---
+++ src/views/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: '欢迎使用vue-common模板'
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+  font-size: 1.2em;
+}
+</style>
