bd6b0b02411495ad674046b4d2f4e95d5f3f36b7
kangting <kangting@daojia.com>
2018-08-18 14:11:42 +0800

    底部

---
+++ src/framework/Foot.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+    <span>首页</span>
+    <span>分类</span>
+    <span>购物车</span>
+    <span>我的</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'footer'
+  };
+</script>
+
+<style scoped lang="scss" >
+  .footer{
+    display: flex;
+    justify-content: space-around;
+    span{
+      padding-top: 20px;
+    }
+  }
+</style>
